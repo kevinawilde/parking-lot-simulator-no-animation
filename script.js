5fffb4787fb4efc706f8479d8b88ca7497af80c0
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-04-09 20:35:28 -0500

    car factory working

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const number_of_cars = 50;
 const number_of_spaces = 10;
 
 let waitingList = [];
+let goneList;
 let parkingLot = new Array(number_of_spaces).fill('_');
 
 
-let makelist = ["Honda","Hyundai","Kia","Mazda","Nissan","Ford", "Chevrolet","Chrysler","Jeep","Dodge"];
+let makeModelList = [["Honda","Civic","Accord","Fit","CR-V","Pilot","Odyssey","Ridgeline"],
+					["Hyundai","Kona", "Tucson","Santa Fe","Elantra","Sonata","Accent","Veloster"],
+					["Kia","Soul","Sorento","Niro","Sedona","Sportage"],
+					["Mazda","Mazda3","Mazda6","CX-3","CX-5","CX-9","Miata"],
+					["Nissan","Versa","Sentra","Altima","Maxima","Leaf","Rogue","Murano","Armada","Pathfinder"],
+					["Ford","Fiesta","Focus","Fusion","Taurus","Escape","Edge","Flex","Explorer","Expedition","F-150","Ranger"], 
+					["Chevrolet","Spark","Sonic","Cruze","Malibu","Impala","Volt","Trax","Equinox","Tahoe","Suburban","Colorado","Silverado"],
+					["Chrysler","300","Pacifica"],
+					["Jeep","Wrangler","Grand Cherokee","Cherokee","Compass","Renegade","Gladiator"],
+					["Dodge","Charger","Challenger","Durango","Journey","Grand Caravan"]
+					];
 
 let colorList = ["Black","Silver","Red","Yellow","White","Blue","Green","Orange"];
 
@@ -34,12 +45,20 @@ function Car(make,model,year,color,id) {
 	this.color = color;
 	this.id = id;
 }
+function modelListSelector(listName){}
 function carFactory(numberOfCars) {
-	for(let i = 0; i < number_of_cars; i++)
-	let color = colorList[Math.floor(Math.random()*colorList.length)];
+	for(let i = 0; i < number_of_cars; i++){0
+		var color = colorList[Math.floor(Math.random()*colorList.length)];
+		var makeList = makeModelList[Math.floor(Math.random()*makeModelList.length)];
+		var make = makeList[0];
+		var models = makeList[Math.floor(Math.random()*(makeList.length - 1) +1)];
+		var year = Math.floor(Math.random()*(2019-2000) +2000);
+		var id = Math.floor(Math.random()*(1000) +1);
+		var car = new Car(make,models,year,color,id);
+		waitingList.push(car);
+	}
 
 }
+carFactory(number_of_cars);
+console.log(waitingList);
 
-console.log(parkingLot)
-
-console.log(colorList[Math.floor(Math.random()*colorList.length)])
